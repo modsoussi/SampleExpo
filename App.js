cf3919f4fc0932c92dafe2dd16f07795fb18af36
Mohamed Soussi <mohameddsoussi@gmail.com>
2020-05-13 02:04:12 +0100

    ready to test

--- App.js
+++ App.js
@@ -1,10 +1,52 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback } from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Constants from 'expo-constants';
+import { Linking } from 'expo';
 
 export default function App() {
+  const handleRedirect = ({ url }) => {
+    if (Constants.platform.ios) {
+      WebBrowser.dismissBrowser();
+    } else {
+      removeLinkingListener();
+    }
+
+    let data = Linking.parse(url);
+    console.log(data);
+  }
+
+  const addLinkingListener = () => {
+    Linking.addEventListener('url', handleRedirect);
+  }
+
+  const removeLinkingListener = () => {
+    Linking.removeEventListener('url', handleRedirect);
+  }
+
+  const onPress = useCallback(async () => {
+    addLinkingListener();
+
+    try {
+      const result = await WebBrowser.openBrowserAsync(
+        'https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_HF4KzTyLQxu32PlIbsP1BOyQCHQQmr0f&scope=read_write',
+      );
+  
+      if (Constants.platform.ios) {
+        removeLinkingListener();
+      }
+
+      console.log(result);
+    } catch (err) {
+      console.error(err);
+    }
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button 
+        onPress={onPress}
+      />
     </View>
   );
 }
